f4978154fa6526dbdcddc1366119748479a24f87
andreicontor <73476784+andreicontor@users.noreply.github.com>
2022-04-01 22:41:11 +0300

    Add files via upload

---
+++ Card/Maestro.java
@@ -0,0 +1,9 @@
+package Card;
+
+public class Maestro extends Card
+{
+    public Maestro(int cardId, String IBAN, String name)
+    {
+        super(cardId, IBAN, name);
+    }
+}
---
+++ Card/MasterCard.java
@@ -0,0 +1,9 @@
+package Card;
+
+public class MasterCard extends Card
+{
+    public MasterCard(int cardId, String IBAN, String name)
+    {
+        super(cardId, IBAN, name);
+    }
+}
