5b63fb340885add03f524f0d35c2c3800175eb07
andreicontor <73476784+andreicontor@users.noreply.github.com>
2022-04-29 00:27:54 +0300

    Update Main.java

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+/*package com.company;
 
 import Accounts.Account;
 import Accounts.Transaction;
@@ -75,5 +75,83 @@ public class Main {
             }
         }
 
+    }
+}
+*/
+
+
+
+
+package com.company;
+
+import Accounts.Account;
+import Accounts.Account_Singleton;
+import Accounts.Transaction;
+import Accounts.Transaction_Singleton;
+import Card.Visa;
+import Client.Client;
+import Client.*;
+
+import Client.Client_Singleton;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    static List<String> availableCommands = Arrays.asList("create_client", "create_client_card", "get_client",
+            "get_client_amount", "get_client_accounts", "load_client_account",
+            "create_transaction", "create_client_account", "close_client_account",
+            "get_client_transactions", "help", "end");
+    static List<String> commandsDescriptions = Arrays.asList("creare cont client", "creare card client", "afis detalii client", "obtinere sold client",
+            "Preluare conturi client", "Încărcare cont client", "Creeare tranzacție", "Creare cont client",
+            "Închidere cont client", "Preluare transacții client",
+            "Afișează comenzi", "Finalizare");
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean end = false;
+        ServiceClass mainService = new ServiceClass();
+        Audit auditService = new Audit();
+        Client_Singleton.getInstance().loadFromCSV();
+        Account_Singleton.getInstance().loadFromCSV();
+        Transaction_Singleton.getInstance().loadFromCSV();
+        mainService.setClients(Client_Singleton.getInstance().getCustomers());
+        mainService.setAccounts(Account_Singleton.getInstance().getAccounts());
+        mainService.setTransactions(Transaction_Singleton.getInstance().getTransactions());
+        mainService.linkAccounts();
+        while (!end) {
+            System.out.println("Insert command: (help - see commands)");
+            String command = in.nextLine().toLowerCase(Locale.ROOT);
+            try {
+                switch (command) {
+                    case "create_client" -> mainService.createClient(in);
+                    case "create_client_card" -> mainService.createClientCard(in);
+                    case "get_client" -> mainService.getClient(in);
+                    case "get_client_amount" -> mainService.getClientAmount(in);
+                    case "get_client_accounts" -> mainService.getClientAccounts(in);
+                    case "get_client_account" -> mainService.getClientAccount(in);
+                    case "load_client_account" -> mainService.loadClientAccount(in);
+                    case "create_transaction" -> mainService.createTransaction(in);
+                    case "create_client_account" -> mainService.createClientAccount(in);
+                    case "close_client_account" -> mainService.closeAccount(in);
+                    case "get_client_transactions" -> mainService.getClientTransactions(in);
+                    case "end" -> end = true;
+                }
+                if (availableCommands.contains(command))
+                    auditService.logAction(command);
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        }
+        Client_Singleton.getInstance().setCustomers(mainService.getClients());
+        Account_Singleton.getInstance().setAccounts(mainService.getAccounts());
+        Transaction_Singleton.getInstance().setTransactions(mainService.getTransactions());
+        Client_Singleton.getInstance().dumpToCSV();
+        Account_Singleton.getInstance().dumpToCSV();
+        Transaction_Singleton.getInstance().dumpToCSV();
     }
 }
