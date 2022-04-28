c9564489710d11170cde77eec2fb03f2a10708ae
andreicontor <73476784+andreicontor@users.noreply.github.com>
2022-04-29 00:34:04 +0300

    Update AccountGeneration.java

--- Accounts/AccountGeneration.java
+++ Accounts/AccountGeneration.java
@@ -4,6 +4,10 @@ public class AccountGeneration {
 
     private static int id = 0;
 
+    public static void incrementUniqueId(int inc) {
+        AccountGeneration.id += inc;
+    }
+
     public Account createAccount(String name, int customerId)
     {
         return new Account(name, customerId, id++);
